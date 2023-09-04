7b6b3459df47ccdf7da17515492c76502b9dcc0f
fang duan <df1228@gmail.com>
2023-09-04 11:21:34 +0800

    chore: update lefthook

--- .lefthook/commit-msg/commit-lint.sh
+++ .lefthook/commit-msg/commit-lint.sh
@@ -3,6 +3,10 @@
 # https://www.conventionalcommits.org/en/v1.0.0/
 # https://github.com/conventionalcommit/commitlint#available-rules
 
+# alternative
+# https://github.com/go-semantic-release/semantic-release#how-does-it-work
+# https://github.com/hazcod/semantic-commit-hook
+
 if ! type commitlint >/dev/null 2>/dev/null; then
 	echo ""
     echo "commitlint could not be found"
